0255b7198b0c9bf13fbb498c4f3c474d0446dbcb
wormi <andreas.worm@headissue.com>
2020-09-23 12:17:41 +0200

    remove generated assets

--- src/main/resources/static/static/js/xlsx.70abcdf0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpmap-pins"]=this["webpackJsonpmap-pins"]||[]).push([[2],{230:function(n,p){},320:function(n,p){},321:function(n,p){}}]);
-//# sourceMappingURL=xlsx.70abcdf0.chunk.js.map
